a5be8d6a4b53df35649501c536d555b01e0ed62f
Umer Dar <umerbashirdar@gmail.com>
2017-04-30 23:22:19 -0500

    Big commit with django project startup and more getting rolling with Py

---
+++ house_of_trees/basics/functions.py
@@ -0,0 +1,18 @@
+def hows_the_parrot():
+	print("WAT")
+
+# hows_the_parrot()
+
+def lumberjack(name):
+	if name.lower() == 'umer':
+		print("Umer's a lumberjack and he's the man.")
+	else:
+		print("{} sleeps all night and works all day!".format(name))
+
+lumberjack("sleepy")		
+
+def counter(count):
+	print("Hi " * count)
+
+counter(3)	
+
---
+++ house_of_trees/basics/loopin.py
@@ -0,0 +1,13 @@
+# names = ['Moneeb', 'Umer', 'Gagan', 'KJ', 'Happy']
+# for idiot in names:
+# 	if idiot == 'Gagan':
+# 		break
+# 	print(idiot)	
+
+# names = ['Moneeb', 'Umer', 'Gagan', 'KJ', 'Happy']
+# for idiot in names:
+# 	if idiot == 'Gagan':
+# 		continue
+# 	print(idiot)		
+
+input("what u age?")
